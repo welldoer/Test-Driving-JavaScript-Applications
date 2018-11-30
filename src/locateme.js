0b77ef813fdc8804bc02b09242341e1f7ea17627
welldoer <welldoer@263.net>
2018-11-30 14:06:24 +0800

    丰富 createURL() 测试。

--- src/locateme.js
+++ src/locateme.js
@@ -1,3 +1,5 @@
 var createURL = function( latitude, longitude ) {
+  if( latitude == undefined || longitude == undefined )
+    return '';
   return 'http://maps.google.com?q=' + latitude + ',' + longitude;
 }
